844d4d06578b5380dae5f4f5e0e6452d5d936c7d
P�mella Maria Mendes <54576262882@>
2024-10-04 16:18:33 -0300

    first commit

---
+++ api/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from.models import Produto
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Produto
+        fiels = [
+            'id', 
+            'codigoProduto',
+            'tituloProduto',
+            'preco',
+            'descricao',
+            'imagemProduto',
+            'categoriaProduto',
+            'classProduto',
+            'exibirHome',
+        ]
+        
